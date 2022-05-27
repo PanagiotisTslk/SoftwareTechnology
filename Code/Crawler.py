a4804f3b86a8211fb211de042b3cbf678d0bfef1
dkaptheprogrammer <101343244+dkaptheprogrammer@users.noreply.github.com>
2022-05-27 14:37:29 +0300

    Update Crawler

---
+++ Code/Crawler.py
@@ -0,0 +1,15 @@
+''' Script for Class Crawler '''
+
+# Libraries
+
+
+# Class for students
+class Crawler:
+  
+  # Constructor
+  def __init__(self, websites, kind):
+    self.websites = websites
+    self.kind = kind
+
+  def ActivateCrawler(self):
+    pass
