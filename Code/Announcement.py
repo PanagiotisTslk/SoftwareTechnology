563cdb4b2f37fc33c779f3d9d2b5d5c734032624
George Thanos <101195913+georgethanos@users.noreply.github.com>
2022-05-28 18:21:55 +0300

    Updated With Crawling Process Code

--- Code/Announcement.py
+++ Code/Announcement.py
@@ -74,3 +74,80 @@ def get_database():
     # Create the database
     return client["StudentUp"]
 
+
+### Crawling Process ###
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+##--- Extract Links ---## 
+
+# Upatras url page
+upatras = "https://www.upatras.gr/category/news/"
+
+# Take the contents of a page
+with urllib.request.urlopen(upatras) as response:
+	   html = response.read()
+
+# Take page contents with beautifulsoup
+soup = BeautifulSoup(html, features="html.parser")
+
+# Get specific tag from html page
+announcements = soup.find_all('a', {"class":"_self"})
+
+# List that contains url for announcements pages
+url_list = []
+
+# Extract urls for announcements pages
+for url in announcements:
+    url_list.append(url.get("href"))
+
+##--- Extract Content from Urls ---##
+
+# List with dictionaries that contain 1.Title, 2.Body, 3.Author, 4.url of announcement
+content_list = []
+    
+for url in url_list:
+    # Take the contents of a page
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+
+    # Take page contents with beautifulsoup
+    soup = BeautifulSoup(html, features="html.parser")
+
+    # Extract text from specific html tags
+    # Get Announcement: 1.Title, 2.Body, 3.Author
+    html_title = soup.find_all('h1', {'class':'entry-title'}) # Title
+    html_body = soup.find_all('div', {'class':'entry-content'}) # Body
+    html_author = soup.find_all('a', {'class':'url fn n'})  # Author
+
+    title = ""
+    body = ""
+    author = ""
+
+    # Take only the text without html tags and metadata
+    for content in html_title:
+        title = content.text
+
+    for content in html_body:
+        body += content.text + " "
+
+    for content in html_author:
+        author = content.text
+
+    contents = {
+        "title": title,
+        "body": body,
+        "author": author,
+        "url": url
+    }
+
+    content_list.append(contents)
+
+
+
+# Parse specific contents of content_list
+# Get title from 2nd announcement
+content_list[1]["title"]
+
+
