f5a080540a0ced6ae2a5387282f45fe8b182dc0f
dkaptheprogrammer <101343244+dkaptheprogrammer@users.noreply.github.com>
2022-05-28 13:20:48 +0300

    Update Crawler.py

--- Code/Crawler.py
+++ Code/Crawler.py
@@ -1,15 +1,32 @@
 ''' Script for Class Crawler '''
 
+
 # Libraries
 
 
 # Class for students
 class Crawler:
-  
-  # Constructor
-  def __init__(self, websites, kind):
-    self.websites = websites
-    self.kind = kind
 
-  def ActivateCrawler(self):
-    pass
+    # Constructor
+    def __init__(self, websites, kind):
+        self.websites = websites
+        self.kind = kind
+
+    def ActivateCrawler(self):
+        pass
+
+
+# Connection with database
+def get_database():
+
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = "mongodb+srv://tslk:Tslkgtx950pt@cluster0.jnvsh.mongodb.net/StudentUp"
+
+    # Create a connection using MongoClient
+    from pymongo import MongoClient
+    client = MongoClient(CONNECTION_STRING)
+
+    # Create the database
+    return client["StudentUp"]
+
+
