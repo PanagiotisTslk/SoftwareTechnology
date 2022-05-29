cd6cf9423e3749ec3d71c25eb42de16607716754
George Thanos <101195913+georgethanos@users.noreply.github.com>
2022-05-29 21:37:48 +0300

    Update Note.py
    Added content in UpdateNote()

--- Code/Note.py
+++ Code/Note.py
@@ -155,8 +155,87 @@ class Note:
 
         return notes_list
 
-    def UpdateNote(self):
-        pass
+    def UpdateNote(self, course, notename, newFilename, newDescription, newNotename):
+        
+        sizeCheck = False
+
+        # Check size of file
+        sizeCheck = self.CheckFileSize(newFilename)
+
+        # If file size is ok the proceed with the updates needed
+        if sizeCheck is True:
+
+            ''' Get old note filename and update DataBase Note Document '''
+            
+            # Connecting with database
+            dbname = get_database() 
+            # Choosing Note collection
+            col = dbname["Note"]
+
+            for n in col.find():
+                # Retrieve old note filename that will be deleted from Google Drive
+                # and old: notename, description and timestamp.
+                if n['notename'] == notename:
+                    oldNotename = n['notename']
+                    oldFilename = n['filename']
+                    oldDescr = n['description']
+                    oldTimestamp = n['timestamp']
+                    
+            # Find new timestamp
+            timestampNow = datetime.now().strftime('%d-%m-%Y')
+
+            # DataBase update for note notename
+            queryNotename = { 'notename': oldNotename } 
+            newValues_Notename = { "$set": { "notename": newNotename } }
+
+            # DataBase update for note description
+            queryDescr = { 'description': oldDescr }
+            newValues_Descr = { "$set": { "description": newDescription } }
+
+            # DataBase update for note timestamp
+            queryTimestamp = { 'timestamp': oldTimestamp }
+            newValues_Timestamp = { "$set": { "timestamp": timestampNow } }
+
+            # Make the updates
+            col.update_one(queryNotename, newValues_Notename)
+            col.update_one(queryDescr, newValues_Descr)
+            col.update_one(queryTimestamp, newValues_Timestamp)
+
+            ''' Delete old file in Google Drive '''
+
+            # Connecting with database
+            dbname = get_database() 
+            # Choosing Courses collection
+            col = dbname["Courses"]
+
+            for n in col.find():
+                # Retrieve drive folder id from Courses Collection
+                if n['name'] == course:
+                    folder_id = n['driveID']
+
+            # Connection with Google Drive
+            drive = drive_connection()
+
+            # Get list of all files inside folder
+            file_list = drive.ListFile({'q': "'{}' in parents and trashed=false".format(folder_id)}).GetList()
+
+            # Find correct file
+            for file in file_list:
+                if file["title"] == oldFilename:
+                    # Delete file permanently
+                    file.Delete()
+
+            ''' Upload new note file in Google Drive '''
+
+            # Select Google Drive folder to upload file
+            gfile = drive.CreateFile({'parents': [{'id': folder_id}]})
+            # Read file and upload it to folder.
+            gfile.SetContentFile(newFilename)
+            gfile.Upload()
+
+            print("Note updated successfully!")
+
+        return sizeCheck
 
     def DeleteNote(self, course, notename, filename):
         # Delete note file from Google Drive
