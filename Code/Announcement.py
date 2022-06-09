6dbe50143eeb8ead10f1aeb1a6f6226f3722645f
dkaptheprogrammer <101343244+dkaptheprogrammer@users.noreply.github.com>
2022-06-09 19:34:20 +0300

    Update Announcement.py

--- Code/Announcement.py
+++ Code/Announcement.py
@@ -1,64 +1,180 @@
 ''' Script for Class Announcement '''
 
 # Libraries
-from datetime import datetime
-import Crawler.py
+import Crawler
 
 
 # Class for evaluations
 class Announcement:
 
     # Constructor
-    def __init__(self, timestamp, author, text, website, kind):
-        self.timestamp = timestamp
+    def __init__(self, title, author, body, url):
+        self.title = title
         self.author = author
-        self.text = text
-        self.website = website
-        self.kind = kind
+        self.body = body
+        self.url = url
 
-    # Taking an evaluation object and storing it to database
-    def StoreAnnouncements(self, website):
+    # Taking an announcement object and storing it to database
+    def StoreAnnouncements(self):
 
         # Connecting with database
         dbname = get_database()
         # Choosing collection
         col = dbname["Announcements"]
 
-        announcement = {
-            "author": self.author,
-            "text": self.text,
-            "website": website,
-            "kind": self.kind,
-            "timestamp": datetime.now()
-        }
+        ### Crawling Process ###
 
-        col.insert_one(announcement)
-        print("Announcement stored successfully")
+        import urllib.request
+        from bs4 import BeautifulSoup
 
-    # Retrieving all evaluations of a course given
+        ##--- Extract Links ---##
+
+        # Upatras url page
+        upatras = "https://www.upatras.gr/category/news/"
+
+        # Take the contents of a page
+        with urllib.request.urlopen(upatras) as response:
+            html = response.read()
+
+        # Take page contents with beautifulsoup
+        soup = BeautifulSoup(html, features="html.parser")
+
+        # Get specific tag from html page
+        announcements = soup.find_all('a', {"class": "_self"})
+
+        # List that contains url for announcements pages
+        url_list = []
+
+        # Extract urls for announcements pages
+        for url in announcements:
+            url_list.append(url.get("href"))
+
+        ##--- Extract Content from Urls ---##
+
+        for url in url_list:
+            # Take the contents of a page
+            with urllib.request.urlopen(url) as response:
+                html = response.read()
+
+            # Take page contents with beautifulsoup
+            soup = BeautifulSoup(html, features="html.parser")
+
+            # Extract text from specific html tags
+            # Get Announcement: 1.Title, 2.Body, 3.Author
+            html_title = soup.find_all('h1', {'class': 'entry-title'})  # Title
+            html_body = soup.find_all('div', {'class': 'entry-content'})  # Body
+            html_author = soup.find_all('a', {'class': 'url fn n'})  # Author
+
+            title = ""
+            body = ""
+            author = ""
+
+            # Take only the text without html tags and metadata
+            for content in html_title:
+                title = content.text
+
+            for content in html_body:
+                body += content.text + " "
+
+            for content in html_author:
+                author = content.text
+
+        # Parse specific contents of announcement
+        # Store announcements in database
+
+            announcement = {
+                "author": author,
+                "body": body,
+                "url": url,
+                "title": title
+            }
+
+            col.insert_one(announcement)
+            print("Announcement stored successfully")
+
+    # Retrieving all announcements
     def RetrieveAnnouncements(self):
+        aCrawler = Crawler.Crawler('', '', '', '')
+        aCrawler.ActivateCrawler()
+        print('\n')
 
-        # Connecting with database
-        dbname = get_database()
-        # Choosing collection
-        col = dbname["Announcements"]
+    def ReclassifyAnnouncements(self, author_called):
+        ### Crawling Process ###
 
-        for c in col.find():
-            if c['website'] == self.website:
-                self.author = c['author']
-                self.text = c['text']
-                self.website = c['website']
-                self.kind = c['kind']
-                self.timestamp = c['timestamp']
+        import urllib.request
+        from bs4 import BeautifulSoup
 
-    def ReclassifyAnnouncements(self):
-        pass
+        ##--- Extract Links ---##
 
-    def CheckSourceAnnouncements(self):
-        pass
+        # Upatras url page
+        upatras = "https://www.upatras.gr/category/news/"
 
-    def LoadPage(self):
-        pass
+        # Take the contents of a page
+        with urllib.request.urlopen(upatras) as response:
+            html = response.read()
+
+        # Take page contents with beautifulsoup
+        soup = BeautifulSoup(html, features="html.parser")
+
+        # Get specific tag from html page
+        announcements = soup.find_all('a', {"class": "_self"})
+
+        # List that contains url for announcements pages
+        url_list = []
+
+        # Extract urls for announcements pages
+        for url in announcements:
+            url_list.append(url.get("href"))
+
+        ##--- Extract Content from Urls ---##
+
+        # List with dictionaries that contain 1.Title, 2.Body, 3.Author, 4.url of announcement
+        content_list = []
+
+        for url in url_list:
+            # Take the contents of a page
+            with urllib.request.urlopen(url) as response:
+                html = response.read()
+
+            # Take page contents with beautifulsoup
+            soup = BeautifulSoup(html, features="html.parser")
+
+            # Extract text from specific html tags
+            # Get Announcement: 1.Title, 2.Body, 3.Author
+            html_title = soup.find_all('h1', {'class': 'entry-title'})  # Title
+            html_body = soup.find_all('div', {'class': 'entry-content'})  # Body
+            html_author = soup.find_all('a', {'class': 'url fn n'})  # Author
+
+            title = ""
+            body = ""
+            author = ""
+
+            # Take only the text without html tags and metadata
+            for content in html_title:
+                title = content.text
+
+            for content in html_body:
+                body += content.text + " "
+
+            for content in html_author:
+                author = content.text
+
+            if author_called == author:
+                contents = {
+                    "title": title,
+                    "body": body,
+                    "author": author,
+                    "url": url
+                }
+
+                content_list.append(contents)
+
+        # Parse specific contents of content_list
+        # Get urls from announcements
+
+        for i in range(0, len(content_list)):
+            print(f"Url of announcement with author {content_list[i]['author']} is {content_list[i]['url']}")
+        print('\n')
 
 
 # Connection with database
@@ -75,79 +191,7 @@ def get_database():
     return client["StudentUp"]
 
 
-### Crawling Process ###
-
-import urllib.request
-from bs4 import BeautifulSoup
-
-##--- Extract Links ---## 
-
-# Upatras url page
-upatras = "https://www.upatras.gr/category/news/"
-
-# Take the contents of a page
-with urllib.request.urlopen(upatras) as response:
-	   html = response.read()
-
-# Take page contents with beautifulsoup
-soup = BeautifulSoup(html, features="html.parser")
-
-# Get specific tag from html page
-announcements = soup.find_all('a', {"class":"_self"})
-
-# List that contains url for announcements pages
-url_list = []
-
-# Extract urls for announcements pages
-for url in announcements:
-    url_list.append(url.get("href"))
-
-##--- Extract Content from Urls ---##
-
-# List with dictionaries that contain 1.Title, 2.Body, 3.Author, 4.url of announcement
-content_list = []
-    
-for url in url_list:
-    # Take the contents of a page
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-
-    # Take page contents with beautifulsoup
-    soup = BeautifulSoup(html, features="html.parser")
-
-    # Extract text from specific html tags
-    # Get Announcement: 1.Title, 2.Body, 3.Author
-    html_title = soup.find_all('h1', {'class':'entry-title'}) # Title
-    html_body = soup.find_all('div', {'class':'entry-content'}) # Body
-    html_author = soup.find_all('a', {'class':'url fn n'})  # Author
-
-    title = ""
-    body = ""
-    author = ""
-
-    # Take only the text without html tags and metadata
-    for content in html_title:
-        title = content.text
-
-    for content in html_body:
-        body += content.text + " "
-
-    for content in html_author:
-        author = content.text
-
-    contents = {
-        "title": title,
-        "body": body,
-        "author": author,
-        "url": url
-    }
-
-    content_list.append(contents)
-
-
-
-# Parse specific contents of content_list
-# Get title from 2nd announcement
-content_list[1]["title"]
-
-
+aAnnouncement = Announcement('', '', '', '')
+aAnnouncement.RetrieveAnnouncements()
+aAnnouncement.ReclassifyAnnouncements('admin')
+aAnnouncement.StoreAnnouncements()
